d1190ff7ff308790b3a554f7cef6c3a1e4842a9a
jpwilson <jpwilson@hey.com>
2025-04-05 21:13:10 -0500

    fix env vars for api PROPERLY AGAIN!

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,4 +1,5 @@
 export const corsHeaders = {
   'Access-Control-Allow-Origin': '*', // Allow requests from any origin (adjust in production!)
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+  'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS', // Explicitly allow methods
 } 
